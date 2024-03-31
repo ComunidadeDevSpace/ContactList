fa678dacca6cda767d4a6875643a58f7512ca47e
Roque Buarque <roqueb@spotify.com>
2024-03-31 11:49:36 +0100

    click listener

--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.devspace.recyclerview
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val adapter by lazy {
+        ContactsAdapter()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,92 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val rvList = findViewById<RecyclerView>(R.id.rv_list)
+        rvList.layoutManager = LinearLayoutManager(this)
+        rvList.adapter = adapter
+        adapter.submitList(contacts)
+
+        adapter.setOnClickListener {
+            val intent = Intent(this, ContactDetailActivity::class.java)
+            intent.putExtra("name", it.name)
+            intent.putExtra("phone", it.phone)
+            intent.putExtra("image", it.image)
+            startActivity(intent)
+        }
     }
 }
+
+private val contacts = listOf(
+    Contact(
+        name = "Roque",
+        phone = "(55)11 98756-7584",
+        image = R.drawable.sample2
+    ),
+    Contact(
+        name = "Bruna",
+        phone = "(55)11 92256-7444",
+        image = R.drawable.sample1
+    ),
+    Contact(
+        name = "Brenda",
+        phone = "(55)11 92356-7384",
+        image = R.drawable.sample3
+    ),
+    Contact(
+        name = "Julia",
+        phone = "(55)11 98326-7464",
+        image = R.drawable.sample4
+    ),
+    Contact(
+        name = "Camila",
+        phone = "(55)11 99756-3384",
+        image = R.drawable.sample5
+    ),
+    Contact(
+        name = "Francisco",
+        phone = "(55)11 99336-2284",
+        image = R.drawable.sample8
+    ),
+    Contact(
+        name = "Erick",
+        phone = "(55)11 99116-1184",
+        image = R.drawable.sample9
+    ),
+    Contact(
+        name = "Joao",
+        phone = "(55)11 99236-1135",
+        image = R.drawable.sample10
+    ),
+    Contact(
+        name = "Helena",
+        phone = "(55)11 99216-1235",
+        image = R.drawable.sample11
+    ),
+    Contact(
+        name = "Jose",
+        phone = "(55)11 92356-1284",
+        image = R.drawable.sample12
+    ),
+    Contact(
+        name = "Maria",
+        phone = "(55)11 99226-3384",
+        image = R.drawable.sample13
+    ),
+    Contact(
+        name = "Enzo",
+        phone = "(55)11 99336-1974",
+        image = R.drawable.sample14
+    ),
+    Contact(
+        name = "Laura",
+        phone = "(55)11 99212-2135",
+        image = R.drawable.sample15
+    ),
+    Contact(
+        name = "Gabi",
+        phone = "(55)11 99217-1234",
+        image = R.drawable.sample16
+    ),
+
+)
